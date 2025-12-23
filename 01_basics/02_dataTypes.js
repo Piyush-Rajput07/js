2ff3263f3eb7c6ad4397aed88f643d5ef1db2a0f
Piyush-Rajput07 <thakurpiyushsingh.22@gmail.com>
2025-12-23 18:21:52 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version (but now done auto.)
+
+// alert(3 + 3) // we are using nodejs not browser
+
+//ensure code readability should be high
+
+let name = "Piyush"
+let age = "21"
+let loggedIn = false;
+
+// number => 2^53 - 1
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
