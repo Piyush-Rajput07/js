f72b7e34df216b8fe402e8c2f08471ed85f65e8e
Piyush-Rajput07 <thakurpiyushsingh.22@gmail.com>
2025-12-24 07:15:06 +0000

    comparision operations

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+//console.log(2 > 1)
+
+//comparision oprns => <,>,<=,>=,==,!=
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+//comparision and equality works diff
+//while comparision, null converts to a number, treating it as 0
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined != 0);
+
+// === strict comparision(also checks the datatype of both)
+
+console.log("2" == 2); // true
+console.log("2" === 2); //false as datatype is diff
